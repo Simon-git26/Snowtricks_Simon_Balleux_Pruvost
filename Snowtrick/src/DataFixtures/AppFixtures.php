42a833a8a9e569c9e79660b303dcbac099cdb01d
Simon-git26 <simoncestmoi@hotmail.fr>
2023-03-17 09:55:44 +0100

    Fixtures pour commentaires

--- Snowtrick/src/DataFixtures/AppFixtures.php
+++ Snowtrick/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 // Entité concerné
 use App\Entity\Tricks;
+use App\Entity\Comments;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,7 +13,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        
         // Tricks Fixtures
         for ($i = 1; $i <= 5; $i++) {
             // Instanciations de ma class Trick
@@ -23,6 +23,16 @@ class AppFixtures extends Fixture
             // Object DatetimeInterface et valeur par defaut definit en tant que CURRENT_TIMESTAMP dans mon entité
             $tricks->setDateCreate(new \DateTime);
             $manager->persist($tricks);
+        }
+
+        // Comments Fixtures
+        for ($i = 1; $i <= 5; $i++) {
+            // Instanciations de ma class Trick
+            $comments = new Comments();
+            $comments->setContent('Commentaire '.$i);
+            $comments->setDateCreate(new \DateTime);
+            $comments->setIsActif(1);
+            $manager->persist($comments);
         }
 
         // Comments Fixtures
