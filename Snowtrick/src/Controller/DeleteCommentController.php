4995dbaf90d7d0db69f011b005a395bccb3c0558
Simon-git26 <simoncestmoi@hotmail.fr>
2023-04-10 13:19:34 +0200

    Suppression de commentaire par admin OK

--- Snowtrick/src/Controller/DeleteCommentController.php
+++ Snowtrick/src/Controller/DeleteCommentController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 // Appel de mon entity pour pouvoir utiliser ces method
-use App\Entity\Tricks;
+use App\Entity\Comments;
 // Soumission du formulaire et persistance des données dans la BDD
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,10 +16,8 @@ use Symfony\Component\HttpFoundation\Request;
 // Personalisation de mon formulaire
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-
-class DeleteController extends AbstractController
+class DeleteCommentController extends AbstractController
 {
-
     private $twig;
     private $entityManager;
 
@@ -29,18 +27,18 @@ class DeleteController extends AbstractController
     }
 
     /**
-     * @Route("/delete/{id}", name="app_delete")
+     * @Route("/delete/comment/{id}", name="app_delete_comment")
      */
     public function index(ManagerRegistry $doctrine, int $id, Request $request): Response
     {
         // Mon formulaire de modification de trick
-        $deleteTrick = $doctrine->getRepository(Tricks::class)->find($id);
+        $deleteComment = $doctrine->getRepository(Comments::class)->find($id);
 
-        $form = $this->createFormBuilder($deleteTrick)
+        $form = $this->createFormBuilder($deleteComment)
        
         // Bouton submit
         ->add('submit', SubmitType::class, [
-            'label' => 'Supprimer ce Trick ?',
+            'label' => 'Supprimer ce Commentaire ?',
         ])
         ->getForm();
 
@@ -48,7 +46,7 @@ class DeleteController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->remove($deleteTrick);
+            $this->entityManager->remove($deleteComment);
             $this->entityManager->flush();
             
             // Changer la route plus tard pour /detail/{id}
@@ -57,17 +55,17 @@ class DeleteController extends AbstractController
 
 
         //Permet de recuperer mes données en BDD grace a mes method du Repository et de Doctrine ORM
-        $trick = $doctrine->getRepository(Tricks::class)->find($id);
+        $comment = $doctrine->getRepository(Comments::class)->find($id);
 
-        if (!$trick) {
-            echo "Aucun Trick n'a était récupéré";
+        if (!$comment) {
+            echo "Aucun commentaire n'a était récupéré";
             die();
         }
 
-        return $this->render('delete/index.html.twig', [
-            'controller_name' => 'DeleteController',
-            'trick' => $trick,
-            'delete_trick' => $form->createView()
+        return $this->render('delete_comment/index.html.twig', [
+            'controller_name' => 'DeleteCommentController',
+            'comment' => $comment,
+            'delete_comment' => $form->createView()
         ]);
     }
 }
