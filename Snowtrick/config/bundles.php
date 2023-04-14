5c0a845e484e8083a1a098b5244dc8e396ac4c0c
Simon-git26 <simoncestmoi@hotmail.fr>
2023-04-14 10:24:15 +0200

    Forgot Password OK / mailTrap pour les test email OK

--- Snowtrick/config/bundles.php
+++ Snowtrick/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
