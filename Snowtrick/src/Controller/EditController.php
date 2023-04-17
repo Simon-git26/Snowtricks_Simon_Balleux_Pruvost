fe1bb2b8f3cd00c5bc84d4d42e616235a33ba595
Simon-git26 <simoncestmoi@hotmail.fr>
2023-04-17 11:16:44 +0200

    Modification d'une image d'un trick OK

--- Snowtrick/src/Controller/EditController.php
+++ Snowtrick/src/Controller/EditController.php
@@ -12,6 +12,11 @@ use App\Entity\Tricks;
 // Soumission du formulaire et persistance des données dans la BDD
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+// Image
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 // Personalisation de mon formulaire
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -31,7 +36,7 @@ class EditController extends AbstractController
     /**
      * @Route("/edit/{id}", name="app_edit")
      */
-    public function index(ManagerRegistry $doctrine, int $id, Request $request): Response
+    public function index(ManagerRegistry $doctrine, int $id, Request $request, SluggerInterface $slugger): Response
     {
 
         // Mon formulaire de modification de trick
@@ -41,7 +46,26 @@ class EditController extends AbstractController
         ->add('title')
         ->add('description')
         ->add('groupe')
-        ->add('image')
+
+        // Ajouter un dl d'image
+        ->add('image', FileType::class, [
+            'label' => 'image',
+            'mapped' => false,
+            'required' => false,
+            'data_class' => null,
+
+            'constraints' => [
+                new File([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'image/png',
+                        'image/jpg',
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid image',
+                ])
+            ]
+        ])
+
         ->add('video')
         // Bouton submit
         ->add('submit', SubmitType::class)
@@ -59,6 +83,35 @@ class EditController extends AbstractController
             // Récupérer l'id du user connecté et l'attribuer au champs user
             $userConnected = $this->getUser();
             $editForm->setUser($userConnected);
+
+
+            // Image uploader //
+            /* Recuperer ma propriete image */
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                // Créer un nom pour le fichier
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // Une fois le servide slugger injecté en debut de fonction, on peut s'en servir
+                $safeFilename = $slugger->slug($originalFilename);
+                // Recuperer le nouveau filename qu'il vient de créer, avec son id et son extension
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    // Stocke le fichier au niveau du dossier selectionné ici
+                    $imageFile->move(
+                        $this->getParameter('image_user'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $editForm->setImage($newFilename);
+            }
+
 
 
             $this->entityManager->persist($editForm);
