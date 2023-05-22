396d83dc2aec0ac6889906191617f91ffda1d7f1
Simon-git26 <simoncestmoi@hotmail.fr>
2023-05-22 13:56:36 +0200

    Ok pour Diagramme UML + diapo soutenance

--- Snowtrick/src/Controller/CreateController.php
+++ Snowtrick/src/Controller/CreateController.php
@@ -17,6 +17,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
+//Sotcker mon message en session
+use Symfony\Component\HttpFoundation\Session\Session;
+
 class CreateController extends AbstractController
 {
 
@@ -35,61 +38,73 @@ class CreateController extends AbstractController
      public function index(Request $request, SluggerInterface $slugger): Response
     {
 
+        $session = new Session();
+
         // Mon formulaire de modification de trick
         $createForm = new Tricks();
 
         $form = $this->createForm(CreateFormType::class, $createForm);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted()) {
+            if($form->isValid()) {
+                // Attribué la date de la creation à mon champ date_create
+                $date= new \DateTime;
+                $dateCreate = $form->get('date_create')->getData();
+                $dateCreate = $date;
+                $createForm->setDateCreate($dateCreate);
 
-            // Attribué la date de la creation à mon champ date_create
-            $date= new \DateTime;
-            $dateCreate = $form->get('date_create')->getData();
-            $dateCreate = $date;
-            $createForm->setDateCreate($dateCreate);
-
-            // Récupérer l'id du user connecté et l'attribuer au champs user du form
-            $userConnected = $this->getUser();
-            $idUser = $form->get('user')->getData();
-            $idUser = $userConnected;
-            $createForm->setUser($idUser);
+                // Récupérer l'id du user connecté et l'attribuer au champs user du form
+                $userConnected = $this->getUser();
+                $idUser = $form->get('user')->getData();
+                $idUser = $userConnected;
+                $createForm->setUser($idUser);
 
 
-            
-            // Image uploader //
-            /* Recuperer ma propriete image */
-            $imageFile = $form->get('image')->getData();
+                
+                // Image uploader //
+                /* Recuperer ma propriete image */
+                $imageFile = $form->get('image')->getData();
 
-            if ($imageFile) {
-                // Créer un nom pour le fichier
-                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                // Une fois le servide slugger injecté en debut de fonction, on peut s'en servir
-                $safeFilename = $slugger->slug($originalFilename);
-                // Recuperer le nouveau filename qu'il vient de créer, avec son id et son extension
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+                if ($imageFile) {
+                    // Créer un nom pour le fichier
+                    $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // Une fois le servide slugger injecté en debut de fonction, on peut s'en servir
+                    $safeFilename = $slugger->slug($originalFilename);
+                    // Recuperer le nouveau filename qu'il vient de créer, avec son id et son extension
+                    $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
 
-                try {
-                    // Stocke le fichier au niveau du dossier selectionné ici
-                    $imageFile->move(
-                        $this->getParameter('image_user'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    // ... handle exception if something happens during file upload
+                    try {
+                        // Stocke le fichier au niveau du dossier selectionné ici
+                        $imageFile->move(
+                            $this->getParameter('image_user'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+
+                    // updates the 'brochureFilename' property to store the PDF file name
+                    // instead of its contents
+                    $createForm->setImage($newFilename);
                 }
 
-                // updates the 'brochureFilename' property to store the PDF file name
-                // instead of its contents
-                $createForm->setImage($newFilename);
+
+                $this->entityManager->persist($createForm);
+                $this->entityManager->flush();
+
+                // Ajouter le message de succès à la variable de session
+                $session->getFlashBag()->add('success', 'Votre trick a été créé avec succès.');
+
+                // Changer la route plus tard pour /detail/{id}
+                return $this->redirectToRoute('app_home');
+            } else {
+                // Traitement en cas d'erreur
+                // Ajoutez un message flash pour indiquer l'erreur
+                $this->addFlash('error', 'Une erreur s\'est produite lors de la soumission du formulaire.');
+    
+                return $this->redirectToRoute('app_home'); // Redirection vers la page d'erreur
             }
-
-
-            $this->entityManager->persist($createForm);
-            $this->entityManager->flush();
-
-            // Changer la route plus tard pour /detail/{id}
-            return $this->redirectToRoute('app_home');
         }
 
 
